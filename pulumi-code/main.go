857e84a4a8ebab19ca88958e6a300c4337da7df7
Dom Hallan <dom@domhallan.com>
2024-05-25 15:43:33 -0400

    Updated AWS infrastructure setup
    The code changes involve a significant update to the AWS infrastructure setup. The previous commented-out code has been replaced with a new implementation that includes:
    
    - Addition of configuration variables
    - Enhanced tagging for resources such as VPC, Internet Gateway, Route Table, and Subnet
    - Lookup functionality for specific Amazon Linux AMI
    - Creation of an IAM Role and Policy with full permissions (god mode)
    - Attachment of the policy to the role
    - Creation of an IAM Instance Profile and its association with the EC2 instance
    - Inclusion of egress rules in Security Group configuration
    
    These changes aim to improve resource management, security, and flexibility in our AWS environment.

--- pulumi-code/main.go
+++ pulumi-code/main.go
@@ -1,114 +1,194 @@
 package main
 
-// import (
-//
-//
+import (
+	"encoding/json"
 
-// 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
-// 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
-// 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-// )
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
 
-// func main() {
-// 	pulumi.Run(func(ctx *pulumi.Context) error {
-// 		// Create a new VPC
-// 		vpc, err := ec2.NewVpc(ctx, "vpc", &ec2.VpcArgs{
-// 			CidrBlock: pulumi.String("10.0.0.0/16"),
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Configuration variables
+		instanceType := "t2.micro"
 
-// 		// Create a public subnet
-// 		publicSubnet, err := ec2.NewSubnet(ctx, "publicSubnet", &ec2.SubnetArgs{
-// 			VpcId:               vpc.ID(),
-// 			CidrBlock:           pulumi.String("10.0.1.0/24"),
-// 			MapPublicIpOnLaunch: pulumi.Bool(true),
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Create a VPC
+		vpc, err := ec2.NewVpc(ctx, "mainVpc", &ec2.VpcArgs{
+			CidrBlock: pulumi.String("10.0.0.0/16"),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("main_vpc"),
+			},
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Create an Internet Gateway
-// 		igw, err := ec2.NewInternetGateway(ctx, "igw", &ec2.InternetGatewayArgs{
-// 			VpcId: vpc.ID(),
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Create an Internet Gateway
+		igw, err := ec2.NewInternetGateway(ctx, "mainIgw", &ec2.InternetGatewayArgs{
+			VpcId: vpc.ID(),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("main_igw"),
+			},
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Create a route table
-// 		routeTable, err := ec2.NewRouteTable(ctx, "routeTable", &ec2.RouteTableArgs{
-// 			VpcId: vpc.ID(),
-// 			Routes: ec2.RouteTableRouteArray{
-// 				&ec2.RouteTableRouteArgs{
-// 					CidrBlock: pulumi.String("0.0.0.0/0"),
-// 					GatewayId: igw.ID(),
-// 				},
-// 			},
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Create a Route Table
+		routeTable, err := ec2.NewRouteTable(ctx, "mainRouteTable", &ec2.RouteTableArgs{
+			VpcId: vpc.ID(),
+			Routes: ec2.RouteTableRouteArray{
+				&ec2.RouteTableRouteArgs{
+					CidrBlock: pulumi.String("0.0.0.0/0"),
+					GatewayId: igw.ID(),
+				},
+			},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("main_route_table"),
+			},
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Associate the route table with the public subnet
-// 		_, err = ec2.NewRouteTableAssociation(ctx, "rta", &ec2.RouteTableAssociationArgs{
-// 			SubnetId:     publicSubnet.ID(),
-// 			RouteTableId: routeTable.ID(),
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Create a Subnet
+		subnet, err := ec2.NewSubnet(ctx, "subnet1", &ec2.SubnetArgs{
+			VpcId:               vpc.ID(),
+			CidrBlock:           pulumi.String("10.0.1.0/24"),
+			AvailabilityZone:    pulumi.String("us-east-1a"),
+			MapPublicIpOnLaunch: pulumi.Bool(true),
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("subnet1"),
+			},
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Create a Security Group to allow SSH traffic
-// 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
-// 			VpcId: vpc.ID(),
-// 			Ingress: ec2.SecurityGroupIngressArray{
-// 				&ec2.SecurityGroupIngressArgs{
-// 					Protocol:   pulumi.String("tcp"),
-// 					FromPort:   pulumi.Int(22),
-// 					ToPort:     pulumi.Int(22),
-// 					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
-// 				},
-// 			},
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		_, err = ec2.NewRouteTableAssociation(ctx, "subnet1RouteTableAssociation", &ec2.RouteTableAssociationArgs{
+			SubnetId:     subnet.ID(),
+			RouteTableId: routeTable.ID(),
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Create an IAM policy with access to all services
-// 		_, err = iam.NewPolicy(ctx, "allAccessPolicy", &iam.PolicyArgs{
-// 			Policy: pulumi.String(`{
-// 				"Version": "2012-10-17",
-// 				"Statement": [
-// 					{
-// 						"Action": "*",
-// 						"Effect": "Allow",
-// 						"Resource": "*"
-// 					}
-// 				]
-// 			}`),
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Retrieve the specific Amazon Linux 2023 AMI
+		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
+			Owners: []string{"137112412989"}, // The owner ID for Amazon AMIs
+			Filters: []ec2.GetAmiFilter{
+				{
+					Name:   "image-id",
+					Values: []string{"ami-0bb84b8ffd87024d8"}, // Specific AMI ID for Amazon Linux 2023
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Use the provided Amazon Linux 2 AMI ID
-// 		amiId := "ami-0bb84b8ffd87024d8"
+		// Create an IAM Role
+		role, err := iam.NewRole(ctx, "exampleRole", &iam.RoleArgs{
+			AssumeRolePolicy: pulumi.String(`{
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "ec2.amazonaws.com"
+                        },
+                        "Action": "sts:AssumeRole"
+                    }
+                ]
+            }`),
+		})
+		if err != nil {
+			return err
+		}
 
-// 		// Create an EC2 instance in the public subnet
-// 		_, err = ec2.NewInstance(ctx, "instance", &ec2.InstanceArgs{
-// 			InstanceType: pulumi.String("t2.micro"),
-// 			Ami:          pulumi.String(amiId),
-// 			SubnetId:     publicSubnet.ID(),
-// 			VpcSecurityGroupIds: pulumi.StringArray{
-// 				securityGroup.ID(),
-// 			},
-// 		})
-// 		if err != nil {
-// 			return err
-// 		}
+		// Create an IAM Policy
+		policyJSON, err := json.Marshal(map[string]interface{}{
+			"Version": "2012-10-17",
+			"Statement": []map[string]interface{}{
+				{
+					"Effect":   "Allow",
+					"Action":   "*",
+					"Resource": "*",
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+		policy, err := iam.NewPolicy(ctx, "godMode", &iam.PolicyArgs{
+			Name:        pulumi.String("god_mode_policy"),
+			Description: pulumi.String("A policy with full permissions"),
+			Policy:      pulumi.String(policyJSON),
+		})
+		if err != nil {
+			return err
+		}
 
-// 		return nil
-// 	})
-// }
+		// Attach the policy to the role
+		_, err = iam.NewRolePolicyAttachment(ctx, "rolePolicyAttachment", &iam.RolePolicyAttachmentArgs{
+			Role:      role.Name,
+			PolicyArn: policy.Arn,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create an IAM Instance Profile
+		instanceProfile, err := iam.NewInstanceProfile(ctx, "instanceProfile", &iam.InstanceProfileArgs{
+			Role: role.Name,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create a Security Group
+		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
+			VpcId: vpc.ID(),
+			Ingress: ec2.SecurityGroupIngressArray{
+				&ec2.SecurityGroupIngressArgs{
+					Protocol:   pulumi.String("tcp"),
+					FromPort:   pulumi.Int(22),
+					ToPort:     pulumi.Int(22),
+					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+				},
+			},
+			Egress: ec2.SecurityGroupEgressArray{
+				&ec2.SecurityGroupEgressArgs{
+					FromPort:   pulumi.Int(0),
+					ToPort:     pulumi.Int(0),
+					Protocol:   pulumi.String("-1"),
+					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+				},
+			},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("ssh_access"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create an EC2 Instance
+		_, err = ec2.NewInstance(ctx, "exampleInstance", &ec2.InstanceArgs{
+			Ami:                 pulumi.String(ami.Id),
+			InstanceType:        pulumi.String(instanceType),
+			SubnetId:            subnet.ID(),
+			VpcSecurityGroupIds: pulumi.StringArray{securityGroup.ID()},
+			Tags: pulumi.StringMap{
+				"Name": pulumi.String("example_instance"),
+			},
+			IamInstanceProfile: instanceProfile.Name, // Attach IAM instance profile to EC2 instance
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
