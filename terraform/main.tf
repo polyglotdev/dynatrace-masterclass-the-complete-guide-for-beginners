8c09064c991b218bd2b4190145414f81e2b691c8
Dom Hallan <dom@domhallan.com>
2024-05-24 23:28:40 -0400

    Added Internet Gateway, Route Table and Security Group
    Enhanced the Terraform configuration by adding an Internet Gateway and a Route Table to the VPC. Also added associations between the route table and subnets. Introduced a security group for SSH access with basic ingress and egress rules. Updated subnet resources to map public IP on launch. Finally, attached the new security group to the EC2 instance resource.

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,22 +33,77 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "main_route_table"
+  }
+}
+
 # Subnets
 resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet1_cidr
-  availability_zone = var.availability_zone1
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet1_cidr
+  availability_zone       = var.availability_zone1
+  map_public_ip_on_launch = true
   tags = {
     Name = "subnet1"
   }
 }
 
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main.id
+}
+
 resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet2_cidr
-  availability_zone = var.availability_zone2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet2_cidr
+  availability_zone       = var.availability_zone2
+  map_public_ip_on_launch = true
   tags = {
     Name = "subnet2"
+  }
+}
+
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Security Group
+resource "aws_security_group" "ssh_access" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict this in a production environment
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh_access"
   }
 }
 
@@ -61,9 +116,11 @@ resource "aws_iam_policy" "god_mode" {
 
 # EC2 Instance
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.subnet1.id
+  ami             = data.aws_ami.amazon_linux.id
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.subnet1.id
+  security_groups = [aws_security_group.ssh_access.name]
+
   tags = {
     Name = "example_instance"
   }
