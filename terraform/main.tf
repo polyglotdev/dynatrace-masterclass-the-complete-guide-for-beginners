b76d7e0e4b2274b2b0065740148454312e474080
Dom Hallan <dom@domhallan.com>
2024-05-30 12:45:15 -0400

    Uncommented AWS Terraform configuration
    The previously commented out AWS Terraform configuration has been uncommented. This includes the setup for VPC, subnets, internet gateway, route table, security group, IAM policy and EC2 instance in main.tf file. The output values for these resources have also been uncommented in outputs.tf file. Similarly, the variables used in these configurations are now active as they've been uncommented in variables.tf file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,120 +8,120 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   region = var.aws_region
-# }
+provider "aws" {
+  region = var.aws_region
+}
 
-# # Retrieve the latest Amazon Linux 2 AMI
-# data "aws_ami" "amazon_linux" {
-#   most_recent = true
-#   filter {
-#     name   = "name"
-#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-#   }
-#   filter {
-#     name   = "owner-id"
-#     values = ["137112412989"]
-#   }
-# }
+# Retrieve the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "owner-id"
+    values = ["137112412989"]
+  }
+}
 
-# # VPC
-# resource "aws_vpc" "main" {
-#   cidr_block = var.vpc_cidr
-#   tags = {
-#     Name = "main_vpc"
-#   }
-# }
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "main_vpc"
+  }
+}
 
-# # Internet Gateway
-# resource "aws_internet_gateway" "main" {
-#   vpc_id = aws_vpc.main.id
-#   tags = {
-#     Name = "main_igw"
-#   }
-# }
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main_igw"
+  }
+}
 
-# # Route Table
-# resource "aws_route_table" "main" {
-#   vpc_id = aws_vpc.main.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.main.id
-#   }
-#   tags = {
-#     Name = "main_route_table"
-#   }
-# }
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "main_route_table"
+  }
+}
 
-# # Subnets
-# resource "aws_subnet" "subnet1" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = var.subnet1_cidr
-#   availability_zone       = var.availability_zone1
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "subnet1"
-#   }
-# }
+# Subnets
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet1_cidr
+  availability_zone       = var.availability_zone1
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet1"
+  }
+}
 
-# resource "aws_route_table_association" "subnet1" {
-#   subnet_id      = aws_subnet.subnet1.id
-#   route_table_id = aws_route_table.main.id
-# }
+resource "aws_route_table_association" "subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main.id
+}
 
-# resource "aws_subnet" "subnet2" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = var.subnet2_cidr
-#   availability_zone       = var.availability_zone2
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "subnet2"
-#   }
-# }
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet2_cidr
+  availability_zone       = var.availability_zone2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet2"
+  }
+}
 
-# resource "aws_route_table_association" "subnet2" {
-#   subnet_id      = aws_subnet.subnet2.id
-#   route_table_id = aws_route_table.main.id
-# }
+resource "aws_route_table_association" "subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.main.id
+}
 
-# # Security Group
-# resource "aws_security_group" "ssh_access" {
-#   vpc_id = aws_vpc.main.id
+# Security Group
+resource "aws_security_group" "ssh_access" {
+  vpc_id = aws_vpc.main.id
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] # Restrict this in a production environment
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict this in a production environment
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "ssh_access"
-#   }
-# }
+  tags = {
+    Name = "ssh_access"
+  }
+}
 
-# # IAM Policy
-# resource "aws_iam_policy" "god_mode" {
-#   name        = "god_mode_policy"
-#   description = "A policy with full permissions"
-#   policy      = file("policy.json")
-# }
+# IAM Policy
+resource "aws_iam_policy" "god_mode" {
+  name        = "god_mode_policy"
+  description = "A policy with full permissions"
+  policy      = file("policy.json")
+}
 
-# # EC2 Instance
-# resource "aws_instance" "example" {
-#   ami                    = data.aws_ami.amazon_linux.id
-#   instance_type          = var.instance_type
-#   subnet_id              = aws_subnet.subnet1.id
-#   vpc_security_group_ids = [aws_security_group.ssh_access.id]
+# EC2 Instance
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
-#   tags = {
-#     Name = "example_instance"
-#   }
-# }
+  tags = {
+    Name = "example_instance"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,34 @@
-# variable "aws_region" {
-#   description = "The AWS region to deploy to"
-#   default     = "us-east-1"
-# }
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
+}
 
-# variable "vpc_cidr" {
-#   description = "The CIDR block for the VPC"
-#   default     = "10.0.0.0/16"
-# }
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
 
-# variable "subnet1_cidr" {
-#   description = "The CIDR block for subnet 1"
-#   default     = "10.0.1.0/24"
-# }
+variable "subnet1_cidr" {
+  description = "The CIDR block for subnet 1"
+  default     = "10.0.1.0/24"
+}
 
-# variable "subnet2_cidr" {
-#   description = "The CIDR block for subnet 2"
-#   default     = "10.0.2.0/24"
-# }
+variable "subnet2_cidr" {
+  description = "The CIDR block for subnet 2"
+  default     = "10.0.2.0/24"
+}
 
-# variable "availability_zone1" {
-#   description = "The availability zone for subnet 1"
-#   default     = "us-east-1a"
-# }
+variable "availability_zone1" {
+  description = "The availability zone for subnet 1"
+  default     = "us-east-1a"
+}
 
-# variable "availability_zone2" {
-#   description = "The availability zone for subnet 2"
-#   default     = "us-east-1b"
-# }
+variable "availability_zone2" {
+  description = "The availability zone for subnet 2"
+  default     = "us-east-1b"
+}
 
-# variable "instance_type" {
-#   description = "The instance type for the EC2 instance"
-#   default     = "t2.micro"
-# }
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  default     = "t2.micro"
+}
